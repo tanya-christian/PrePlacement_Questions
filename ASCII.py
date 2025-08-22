032591fe85f54d5001e0501e4c26e21a307a123a
Tanya Christian <tanyachristian19@gmail.com>
2025-08-22 15:27:57 +0530

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -6,7 +6,11 @@ def asciiConversion(strParam):
     for i in strParam:
         ascii_value = ord(i)
         result.append(ascii_value)
-    result = int("".join(str(num) for num in result))
+     #Convert the list of ASCII values to a single integer
+    result = int("".join(str(num) for num in result)) 
+   """Optional: Convert list of ASCII values to a space-separated string
+      Example: "AB" -> [65, 66] -> "65 66"
+      space_separated_result = " ".join(str(num) for num in result)"""
     print(result)
     
 asciiConversion(input("Enter a String/Character:"))
